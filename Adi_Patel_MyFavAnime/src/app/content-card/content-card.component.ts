b824546808380b8f61c2bcc8f54ae07518dbbf84
aadi2497 <w0751432@myscc.ca>
2023-02-03 18:11:42 -0500

    content-card template created for 3 valid list

--- Adi_Patel_MyFavAnime/src/app/content-card/content-card.component.ts
+++ Adi_Patel_MyFavAnime/src/app/content-card/content-card.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Content } from '../helper-files/content-interface';
+import { ContentList } from '../helper-files/content-list';
 
 @Component({
   selector: 'app-content-card',
@@ -6,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./content-card.component.scss']
 })
 export class ContentCardComponent {
+
+  myanime : ContentList = new ContentList();
+
+  anime1 : Content = {
+    id: 0,
+    title: '',
+    description: '',
+    creator: ''
+  }
+
+  constructor(){
+    this.myanime.addFunction(this.anime1);
+  }
+
+  displayMyIndex(index : number): string {
+    return this.myanime.printIndex(index);
 
 }
