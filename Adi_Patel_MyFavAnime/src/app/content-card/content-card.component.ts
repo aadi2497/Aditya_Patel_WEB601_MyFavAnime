f8c24394009c5eeedaf87be42c77157726b25ca7
aadi2497 <w0751432@myscc.ca>
2023-02-03 20:35:30 -0500

    Deleted Content-list from helper-file and Added new Component Content-list

--- Adi_Patel_MyFavAnime/src/app/content-card/content-card.component.ts
+++ Adi_Patel_MyFavAnime/src/app/content-card/content-card.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { Content } from '../helper-files/content-interface';
-import { ContentList } from '../helper-files/content-list';
 
 @Component({
   selector: 'app-content-card',
@@ -8,35 +7,5 @@ import { ContentList } from '../helper-files/content-list';
   styleUrls: ['./content-card.component.scss']
 })
 export class ContentCardComponent {
-
-    myanime : ContentList = new ContentList();
-
-    anime1 : Content = {
-      id: 0,
-      title: 'One Piece',
-      description: 'One Piece is a Japanese manga series written and illustrated by Eiichiro Oda.',
-      creator: 'Shueisha'
-    }
-    anime2 : Content = {
-      id: 1,
-      title: 'Naruto',
-      description: 'Naruto is a Japanese manga series written and illustrated by Masashi Kishimoto.',
-      creator: 'Masashi Kishimoto'
-    }
-    anime3 : Content = {
-      id: 2,
-      title: 'Demon Slayer',
-      description: 'A youth begins a quest to fight demons and save his sister after finding his family slaughtered and his sister turned into a demon.',
-      creator: 'Koyoharu Gotouge'
-    }
-
-    constructor(){
-      this.myanime.addFunction(this.anime1);
-      this.myanime.addFunction(this.anime2);
-      this.myanime.addFunction(this.anime3);
-    }
-
-    displayMyIndex(index : number): string {
-      return this.myanime.printIndex(index);
-  }
+  Content: Content = {} as Content;
 }
