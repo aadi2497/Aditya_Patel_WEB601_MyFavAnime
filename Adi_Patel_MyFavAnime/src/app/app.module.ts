745993b3e0a8e4511f5f87d425111d70859fba0d
aadi2497 <w0751432@myscc.ca>
2023-03-09 19:40:11 -0500

    Message Component and Service Created
    > ng generate component messages
    
    > ng generate service message

--- Adi_Patel_MyFavAnime/src/app/app.module.ts
+++ Adi_Patel_MyFavAnime/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { ContentCardComponent } from './content-card/content-card.component';
 import { ContentListComponent } from './content-list/content-list.component';
 import { FliterPipe } from './fliter.pipe';
 import { HoverAffectDirective } from './hover-affect.directive';
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,8 @@ import { HoverAffectDirective } from './hover-affect.directive';
     ContentCardComponent,
     ContentListComponent,
     FliterPipe,
-    HoverAffectDirective
+    HoverAffectDirective,
+    MessagesComponent
   ],
   imports: [
     BrowserModule
