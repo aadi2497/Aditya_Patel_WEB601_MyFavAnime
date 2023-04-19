eca2faed0ca4e234c0eb32266e5ee71b9fa11308
aadi2497 <w0751432@myscc.ca>
2023-04-19 18:14:02 -0400

    Final Changes

--- Adi_Patel_MyFavAnime/src/app/app-routing.module.ts
+++ Adi_Patel_MyFavAnime/src/app/app-routing.module.ts
@@ -1,12 +1,25 @@
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ContentListComponent } from './content-list/content-list.component';
+import { ContentDetailComponent } from './content-detail/content-detail.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { CommonModule } from '@angular/common';
 
+
+const routes: Routes = [
+  {
+    path: 'content',
+    component: ContentListComponent
+  }
+];
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
